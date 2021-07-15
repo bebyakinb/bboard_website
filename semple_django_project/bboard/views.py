7c31a83108bcfa42b3c9a862f014d62f0d42d97a
bebyakinb <bebyakinb@ya.ru>
2021-07-15 09:24:38 +0300

    add tempate, rename model

--- semple_django_project/bboard/views.py
+++ semple_django_project/bboard/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
+from .models import BboardPost
 
 def index(request):
-    return HttpResponse("Здесь будет выведен список объявлений.")
+    bb_posts = BboardPost.objects.all()
+    return render(request, 'bboard/index.html', {'bb_posts': bb_posts})
